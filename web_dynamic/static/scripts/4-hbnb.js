71b9e41a88466ef3eae19af2ccc1c95e5293bff7
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2024-02-12 00:21:42 +0200

    Re-write of $ in task-5 & update in task-4 0x06

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -44,7 +44,7 @@ function postPlaces (response) {
         return `<article>
                    <div class="title_box">
 	             <h2>{{ place.name }}</h2>
-	             <div class="price_by_night">{{ place.price_by_night }}</div>
+	             <div class="price_by_night">${{ place.price_by_night }}</div>
                    </div>
                    <div class="information">
                      <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
